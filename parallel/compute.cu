4ddc4e1882a759500661f540b487e36002da34c7
Jon O'Connell <jjoc0205@gmail.com>
2023-12-04 18:24:07 -0500

    changed to managed version

--- parallel/compute.cu
+++ parallel/compute.cu
@@ -2,87 +2,87 @@
 #include <math.h>
 #include "vector.h"
 #include "config.h"
-#include <stdio.h>
+#include <cuda_runtime.h>
+#include "cuda.h"
 
-//todo: possibly move Idx/Dim vars to compute function so not defined in each device function??
+//Values and Accels are Global Variables
+vector3* values;
+vector3** accels;
 
-
-__global__ void compute_accels(vector3 **accels, vector3 *hPos, double *mass) {
-	//printf("hjello\n");
-	int i = threadIdx.x + blockIdx.x * blockDim.x;
-	int j = threadIdx.y + blockIdx.y * blockDim.y;
-	int k = threadIdx.z;
-
-	//int i, j, k;
-	printf("i: %d, j: %d, k: %d\n", i, j, k);
-	printf("accels[0][0]: %d", accels[0][0]);
-	//first compute the pairwise accelerations.  Effect is on the first argument.
-	if(i >= NUMENTITIES || j >= NUMENTITIES) {
-		printf("Ending early\n");
-		return;
-	}
-	if (i == j) {
-		//printf("i == j\n");
-		FILL_VECTOR(accels[i][j], 0, 0, 0);
-	} else {
-		printf("Before calcs\n");
-		vector3 distance;
-		distance[k] = hPos[i][k] - hPos[j][k];
-
-		__syncthreads(); // sync to make sure all distances have been calculated
-
-		double magnitude_sq = distance[0] * distance[0] + distance[1] * distance[1] + distance[2] * distance[2];
-		double magnitude = sqrt(magnitude_sq);
-		double accelmag = -1 * GRAV_CONSTANT * mass[j] / magnitude_sq;
-		FILL_VECTOR(accels[i][j], accelmag*distance[0]/magnitude, accelmag*distance[1]/magnitude, accelmag*distance[2]/magnitude);
-		printf("%f\n", accels[i][j][k]);
-	}
-}
-
-__global__ void compute_velocities(vector3 **accels, vector3 *hVel, vector3 *hPos) {
-	/* //int i = threadIdx.x + blockIdx.x * blockDim.x;
-	//int j = threadIdx.y + blockDim.x * gridDim.x; //how many operations to do each group
-	//int k = threadIdx.z;
-	int i=blockIdx.x;
-	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
-	vector3 accel_sum = {0, 0, 0};
-	accel_sum[k] += accels[i][j][k]; //accel_sum local, accels is global
-	//compute the new velocity based on the acceleration and time interval
-	//compute the new position based on the velocity and time interval
-	hVel[i][k] += accel_sum[k] * INTERVAL;
-	hPos[i][k] += hVel[i][k] * INTERVAL; */
+//The compute functions done parallel
+__global__ void parallelCompute(vector3* values, vector3** accels, vector3* d_vel, vector3* d_pos, double* d_mass) {
 	
-	for (int i=0; i < NUMENTITIES; i++){
-		vector3 accel_sum = {0, 0, 0};
-		for (int j=0; j < NUMENTITIES; j++) {
-			for (int k=0; k < 3; k++) {
-				accel_sum[k] += accels[i][j][k];
-			}
+	int currThreadId = blockIdx.x * blockDim.x + threadIdx.x;
+	int i = currThreadId / NUMENTITIES; 								//define i in terms of what block/dimension/thread we are currently using
+	int j = currThreadId % NUMENTITIES; 								//define j in terms of what block/dimension/thread we are currently using
+
+	accels[currThreadId] = &values[currThreadId*NUMENTITIES];
+
+	if(currThreadId < NUMENTITIES * NUMENTITIES) {					//Ensure that the result of our block is actually in bounds
+		if(i == j) {
+			FILL_VECTOR(accels[i][j],0,0,0);
 		}
-		//compute the new velocity based on the acceleration and time interval
-		//compute the new position based on the velocity and time interval
-		for (int k=0; k < 3; k++) {
-			hVel[i][k] += accel_sum[k] * INTERVAL;
-			hPos[i][k] += hVel[i][k] * INTERVAL;
+		else {
+			vector3 distance;
+			
+			distance[0]=d_pos[i][0]-d_pos[j][0];
+			distance[1]=d_pos[i][1]-d_pos[j][1];
+			distance[2]=d_pos[i][2]-d_pos[j][2];
+			double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
+			double magnitude=sqrt(magnitude_sq);
+			double accelmag=-1*GRAV_CONSTANT*d_mass[j]/magnitude_sq;
+			FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
 		}
-	}
-}
 
-//compute: Updates the positions and locations of the objects in the system nbased on gravity.
-//Parameters: None
-//Returns: None
-//Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
-void compute() {	
-	
-	
-	dim3 block_count ((NUMENTITIES+15)/16, (NUMENTITIES+15)/16, 3);
-	dim3 block_size (BLOCK_SIZE, BLOCK_SIZE, 3);
-	
-	//printf("Before kernal call\n");
-	compute_accels<<<block_count, block_size>>>(d_accels, d_hPos, d_mass);
-	//printf("After kernal call\n");
-	compute_velocities<<<NUMENTITIES, 1>>>(d_accels, d_hVel, d_hPos);
-	//DO we need these???
-	cudaMemcpy(hVel, d_hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyDeviceToHost);
-	cudaMemcpy(hPos, d_hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyDeviceToHost);
+		vector3 accel_sum = {(double) *(accels[currThreadId])[0], (double) *(accels[currThreadId])[1], (double) *(accels[currThreadId])[2]};
+
+		d_vel[i][0]+=accel_sum[0]*INTERVAL;
+		d_pos[i][0]=d_vel[i][0]*INTERVAL;
+
+		d_vel[i][1]+=accel_sum[1]*INTERVAL;
+		d_pos[i][1]=d_vel[i][1]*INTERVAL;
+
+		d_vel[i][2]+=accel_sum[2]*INTERVAL;
+		d_pos[i][2]=d_vel[i][2]*INTERVAL;
+
+	}
+
+}
+		
+//Run the three functions that implemenmt parallel design
+void compute() {
+
+
+	//d_hvel and d_hpos hold the hVel and hPos variables on the GPU
+	vector3 *d_vel, *d_pos;
+	double *d_mass;
+
+	cudaMallocManaged((void**) &d_vel, (sizeof(vector3) * NUMENTITIES));
+	cudaMallocManaged((void**) &d_pos, (sizeof(vector3) * NUMENTITIES));
+	cudaMallocManaged((void**) &d_mass, (sizeof(double) * NUMENTITIES));
+
+	//Copy memory from the host onto the GPU
+	cudaMemcpy(d_vel, hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_pos, hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_mass, mass, sizeof(double) * NUMENTITIES, cudaMemcpyHostToDevice);
+
+	//Allocate space on the GPU for these variables
+	cudaMallocManaged((void**) &values, sizeof(vector3)*NUMENTITIES*NUMENTITIES);
+	cudaMallocManaged((void**) &accels, sizeof(vector3*)*NUMENTITIES);
+
+	//Determine number of blocks that we should be running
+	int blockSize = 256;										
+	int numBlocks = (NUMENTITIES + blockSize - 1) / blockSize;
+
+	parallelCompute<<<numBlocks, blockSize>>>(values, accels, d_vel, d_pos, d_mass);
+	cudaDeviceSynchronize();
+
+	//Copy the results back to the device
+	cudaMemcpy(hVel, d_vel, sizeof(vector3) * NUMENTITIES, cudaMemcpyDefault);
+	cudaMemcpy(hPos, d_pos, sizeof(vector3) * NUMENTITIES, cudaMemcpyDefault);
+	cudaMemcpy(mass, d_mass, sizeof(double) * NUMENTITIES, cudaMemcpyDefault);
+
+	cudaFree(accels);
+	cudaFree(values);
+
 }
