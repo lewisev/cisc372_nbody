50ff105e096bc87e244517ae629c8e53c44edc5b
lewisev <lewisev@udel.edu>
2023-12-05 12:14:21 -0500

    try with new compute velocities

--- parallel/compute.cu
+++ parallel/compute.cu
@@ -42,19 +42,18 @@ __global__ void compute_accels(vector3 **accels, vector3 *hPos, double *mass){
 }
 
 __global__ void compute_velocities(vector3 **accels, vector3 *hPos, vector3 *hVel){
-	int i = blockIdx.x;
-	int k = threadIdx.x;
 
-	if(i >= NUMENTITIES) {
-		return;
-	}
+	int i = threadIdx.x + blockIdx.x * blockDim.x;
+	int j = threadIdx.y + blockDim.x * gridDim.x; //how many operations to do each group
+	int k = threadIdx.z;
 
-	double accel_sum = 0;
-	for (int j=0; j < NUMENTITIES; j++){
-		accel_sum += accels[i][j][k];
-	}
-
-	hVel[i][k] += accel_sum * INTERVAL;
+	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
+	vector3 accel_sum = {0, 0, 0};
+	
+	accel_sum[k] += accels[i][j][k]; //accel_sum local, accels is global
+	//compute the new velocity based on the acceleration and time interval
+	//compute the new position based on the velocity and time interval
+	hVel[i][k] += accel_sum[k] * INTERVAL;
 	hPos[i][k] += hVel[i][k] * INTERVAL;
 }
 
@@ -67,6 +66,7 @@ void compute(){
 	dim3 block_size(16,16);
 	dim3 block_count((NUMENTITIES+15) / block_size.x, (NUMENTITIES+15) / block_size.y);
 	
+	dim3 sum_block_size(16,16,3);
 
 	fill_accels<<<1, NUMENTITIES>>>(values, accels);
 	//cudaDeviceSynchronize();
@@ -74,6 +74,6 @@ void compute(){
 	compute_accels<<<block_count, block_size>>>(accels, d_hPos, d_mass);
 	//cudaDeviceSynchronize();
 
-	compute_velocities<<<NUMENTITIES, 3>>>(accels, d_hPos, d_hVel);
+	compute_velocities<<<block_count, sum_block_size>>>(accels, d_hPos, d_hVel);
 	//cudaDeviceSynchronize();
 }
